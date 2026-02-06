7799b24954cbc5ff7229a284c4d8cccff196c361
Rivaldi <rivaldi372@gmail.com>
2026-02-07 03:39:06 +0700

    Initial Commit

---
+++ ImageCutOut/ImageCutOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCutOutApp.swift
+//  ImageCutOut
+//
+//  Created by Rivaldi Kaufman on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageCutOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
